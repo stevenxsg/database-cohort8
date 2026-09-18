63f902850c1ff5f225787a94ff8a1bb592ea0adc
stevenxsg <igwenagustephen131@gmail.com>
2026-09-18 00:53:02 -0700

    fix login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,16 +4,21 @@ const userRouter = express.Router();
 
 const {
     createUser,
-    getUsers ,deleteUser ,getAllUsers ,getSingleUser,
-updateUser
+    getUsers,
+    deleteUser,
+    getAllUsers,
+    getSingleUser,
+    updateUser,
+    loginUser
 } = require("../controllers/usercontroller.js");
 
 userRouter.post("/", createUser);
-userRouter.get("/all-users" ,getAllUsers)
-userRouter.get("/get-one-user/:id" ,getSingleUser)
+userRouter.get("/all-users", getAllUsers);
+userRouter.get("/get-one-user/:id", getSingleUser);
 
 userRouter.delete("/delete-user/:id", deleteUser);
-userRouter.patch("/update-user/:id" , updateUser)
-userRouter.post("/login" ,loginUser)
+userRouter.patch("/update-user/:id", updateUser);
+
+userRouter.post("/login", loginUser);
 
 module.exports = userRouter;
